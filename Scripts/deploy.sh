10e2bb3deaf1086320a9bad917bb653809d50604
Sergeeeek <Sergeeeekg@gmail.com>
2016-02-10 19:54:04 +0300

    Нет смысла собирать под osx

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-ls "$(pwd)/Build/"
-zip -r "$(pwd)/Build/windows.zip" "$(pwd)/Build/windows"
-zip -r "$(pwd)/Build/osx.zip" "$(pwd)/Build/osx"
+zip -r "./Build/windows.zip" "./Build/windows"
