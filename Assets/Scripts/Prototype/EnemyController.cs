98405aed6c65617a7df9ceb2e60e202635539a7c
Kelthalas7 <andreynikitin177@gmail.com>
2016-02-17 09:06:40 +0300

    Тройное оружие, начало AI врагов

---
+++ Assets/Scripts/Prototype/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+	public AnimationCurve _xAnimation;
+	public float _animationScale;
+	public float _downMovementSpeed;
+	public float _animationLengthMultiplier;
+
+	void Update()
+	{
+		_xAnimation.postWrapMode = WrapMode.Loop;
+
+		var movement = -transform.up * _downMovementSpeed;
+		movement.x = _xAnimation.Evaluate(Time.timeSinceLevelLoad) * _animationScale;
+
+		transform.Translate(movement * Time.deltaTime);
+	}
+}
