e60362509eb20eda9154a2525fd406bade58766c
Sergeeeek <Sergeeeekg@gmail.com>
2016-02-20 22:46:54 +0300

    Не нужно устанавливать git-lfs перед сборкой

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -4,20 +4,10 @@
 
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
-echo 'Installing git-lfs'
-curl -o git-lfs.tar.gz https://github.com/github/git-lfs/releases/download/v1.1.1/git-lfs-linux-amd64-1.1.1.tar.gz
-
-tar -xvzf git-lfs.tar.gz
-chmod +x git-lfs-1.1.1/install.sh
-git-lfs-1.1.1/install.sh
-
-git lfs install
-git lfs pull
-
 echo 'Downloading from http://download.unity3d.com/download_unity/linux/unity-editor-5.3.2f1+20160208_amd64.deb'
 curl -o unity.deb http://download.unity3d.com/download_unity/linux/unity-editor-5.3.2f1+20160208_amd64.deb
 
 echo 'Installing Unity'
 sudo dpkg -i unity.deb
 echo 'Fixing dependencies'
-sudo apt-get -f install
+sudo apt-get -y -f install
