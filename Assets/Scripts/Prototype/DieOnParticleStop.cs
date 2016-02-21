58135db0f436dedb3615d2542c6bf85ed2015cbf
Sergeeeek <Sergeeeekg@gmail.com>
2016-02-21 13:09:47 +0300

    Простой скрипт для автоматического удаления эффектов когда все системы частиц останавливаются

---
+++ Assets/Scripts/Prototype/DieOnParticleStop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class DieOnParticleStop : MonoBehaviour
+{
+	List<ParticleSystem> _particleSystems;
+
+	void Start()
+	{
+		_particleSystems = new List<ParticleSystem>(GetComponentsInChildren<ParticleSystem>());
+	}
+
+	void Update()
+	{
+		if(_particleSystems.Count == 0 || _particleSystems.All(x => x.isStopped))
+			Destroy(gameObject);
+	}
+}
