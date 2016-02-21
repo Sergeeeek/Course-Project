f7dd33e05adcc51416a6b9de0fac88624279090f
Sergeeeek <Sergeeeekg@gmail.com>
2016-02-21 13:11:25 +0300

    Начало скрипта для отслеживания состояния игры (очки, game over и т.д.)

---
+++ Assets/Scripts/Prototype/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	GameObject _player;
+	int _score;
+
+	void Start()
+	{
+		_player = GameObject.FindGameObjectWithTag("Player");
+
+		if(_player == null)
+			gameObject.SetActive(false);
+	}
+
+	void Update()
+	{
+		
+	}
+}
