1222ebc3b078689ee0d177aaf1a7163a80f3d8f1
Sergeeeek <Sergeeeekg@gmail.com>
2016-03-28 21:23:35 +0300

    Главное меню
    Анимации, скрипты и уровень для главного меню. Сейчас уже можно выйти из
    игры и запустить первый уровень. Пока нет внутриигрового меню.

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
