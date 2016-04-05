c4852836391bd12b59148e673d7a7bc4a36da5b2
Sergeeeek <Sergeeeekg@gmail.com>
2016-04-05 19:13:39 +0300

    Начал комментировать классы

--- Assets/Scripts/Prototype/Projectile.cs
+++ Assets/Scripts/Prototype/Projectile.cs
@@ -2,16 +2,23 @@
 
 public class Projectile : MonoBehaviour
 {
+	// Префаб создаваемый при попадании пули
 	public GameObject _hitEffect;
+	// Урон от пули
 	public float _damage;
+	// Скорость пули
 	public float _speed;
+	// если true, то когда пуля с чем-нибудь столкнётся или вылетит за экран, то её объект будет 
+	// уничтожен, а не деактивирован
 	public bool _dieOnHit;
 
 	void Update()
 	{
+		// Получаем координаты viewport'а из координат мира этой пули
+		// Координаты viewport'а начинаются в левом верхнем углу экрана (0,0) и заканчиваются в нижнем правом (1,1)
 		var viewPortPos = Camera.main.WorldToViewportPoint(transform.position);
 
-		if(viewPortPos.x > 1f || viewPortPos.x < 0f || viewPortPos.y > 1f || viewPortPos.y < 0f)
+		if(viewPortPos.x > 1f || viewPortPos.x < 0f || viewPortPos.y > 1f || viewPortPos.y < 0f) // если за пределами экрана
 		{
 			DisableOrDestroy();
 			return;
@@ -20,19 +27,21 @@ public class Projectile : MonoBehaviour
 		transform.Translate(Vector3.up * _speed * Time.deltaTime); // Translate двигает объект относительно локальных координат
 	}
 
+	// Функция вызывается когда что-либо объект попадает в триггер, в данном случае корабли в игре являются триггерами
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		var health = other.gameObject.GetComponent<Health>();
-		if(health == null)
+		var health = other.gameObject.GetComponent<Health>(); // Получаем скрипт Health триггера (корабля)
+		if(health == null) // если у объекта нет компонента Health
 			return;
 
+		// Если эта пуля принадлежит игроку и триггер - враг или если эта пуля принадлежит врагу и триггер - игрок, то
 		if((gameObject.tag == "PlayerBullet" && other.gameObject.tag == "Enemy") || (gameObject.tag == "EnemyBullet" && other.gameObject.tag == "Player"))
 		{
-			health.UpdateHealth(-_damage);
-			if(_hitEffect != null)
-				Instantiate(_hitEffect, transform.position, Quaternion.identity);
+			health.UpdateHealth(-_damage); // наносим урон
+			if(_hitEffect != null) // если есть префаб эввекта попадания
+				Instantiate(_hitEffect, transform.position, Quaternion.identity); // то создаём этот эффект в месте попадания
 
-			DisableOrDestroy();
+			DisableOrDestroy(); // Уничтожаем или отключаем объект этой пули
 		}
 	}
 
--- Assets/Scripts/Prototype/ProjectileManager.cs
+++ Assets/Scripts/Prototype/ProjectileManager.cs
@@ -1,42 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic; // Нужно что-бы использовать тип List<T>
+using UnityEngine; // Для классов MonoBehaviour, GameObject, Transform и т.д.
 
 public class ProjectileManager : MonoBehaviour
 {
-	public enum BulletOwner
+	// Нужно для определения владельца пули
+	public enum BulletOwner 
 	{
 		EnemyBullet,
 		PlayerBullet
 	}
 
-	public List<Transform> _shoootPoints = new List<Transform>();
-	public GameObject _projectilePrefab;
+	public List<Transform> _shoootPoints = new List<Transform>(); // Список точек откуда будут появлятся пули
+	public GameObject _projectilePrefab; // Префаб пуль
 
-	public BulletOwner _bulletOwner;
+	public BulletOwner _bulletOwner; // Владелец всех пуль этого ProjectileManager'а
 
-	public float _projectileSpeed;
-	public float _projectileDamage;
-	public bool _alwaysShoot;
-	public float _shootInterval;
+	public float _projectileSpeed; // Скорость пуль
+	public float _projectileDamage; // Урон пуль
+	public bool _alwaysShoot; // Если true, то всегда стреляет, создано для врагов
+	public float _shootInterval; // Интервал между выстрелами в секундах
 
-	public bool _shooting;
+	public bool _shooting; // Если true, то стреляет, сделано для изменения значения их других скриптов, например ShipController
 
-	List<GameObject> _projectilePool;
+	List<GameObject> _projectilePool; // Список пуль
 
-	float _timeFromLastShot;
+	float _timeFromLastShot; // Время с предыдущего выстрела
 
-	void Start()
+	void Start() // Эта функция автоматически вызывается Unity при старте игры
 	{
-		if(_projectilePrefab == null)
-			gameObject.SetActive(false);
+		if(_projectilePrefab == null) // Если префаб пули не установлен в редакторе
+			gameObject.SetActive(false); // Отключаем этот объект
 
-		SetupPool();
+		SetupPool(); // Инициализация списка пуль
 	}
 
-	void Update()
+	void Update() // Эта функция вызывается Unity каждый кадр
 	{
-		_timeFromLastShot += Time.deltaTime;
+		_timeFromLastShot += Time.deltaTime; // Отсчёт времени с предыдущего выстрела, Time.deltaTime = время с предыдущего кадра
 
 		if((_alwaysShoot || _shooting) && _timeFromLastShot >= _shootInterval)
 		{
@@ -46,53 +46,57 @@ public class ProjectileManager : MonoBehaviour
 
 	void SetupPool()
 	{
-		var poolSize = _shoootPoints.Count * (int)Mathf.Round((1f / _shootInterval));
+		var poolSize = _shoootPoints.Count * (int)Mathf.Round((1f / _shootInterval)); // Расчёт начального размера пула
 
 		_projectilePool = new List<GameObject>(poolSize);
 
 		for(int i = 0; i < poolSize; i++)
 		{
-			SpawnProjectile();
+			SpawnProjectile(); // спауним заранее, позже они будут использованы несколько раз
 		}
 	}
-	void Shoot()
+
+	void Shoot() // Стрельба
 	{
+		// Для каждой точки из которой должны появлятся пули
 		foreach(var point in _shoootPoints)
 		{
-			var freeProj = _projectilePool.Find(x => !x.activeInHierarchy);
+			var freeProj = _projectilePool.Find(x => !x.activeInHierarchy); // Находим неактивную пулю в списке
 
-			freeProj = freeProj ?? SpawnProjectile();
+			freeProj = freeProj ?? SpawnProjectile(); // Если такой не нашлось, создаём новую
 
-			freeProj.transform.position = point.position;
+			freeProj.transform.position = point.position; // Устанавливаем начальную позицию и направление пули
 			freeProj.transform.rotation = point.rotation;
 
-			var projComponent = freeProj.GetComponent<Projectile>();
-			projComponent._speed = _projectileSpeed;
+			var projComponent = freeProj.GetComponent<Projectile>(); // Находим скрипт управляющий пулей в объекте
+			projComponent._speed = _projectileSpeed; // И устанавливаем скорость её движения
 
-			freeProj.SetActive(true);
+			freeProj.SetActive(true); // Активизируем объект
 		}
 
-		_timeFromLastShot = 0f;
+		_timeFromLastShot = 0f; // Теперь время с предыдущего выстрела = 0
 	}
 
 	GameObject SpawnProjectile()
 	{
+		// Создаём объект из префаба
 		var obj = Instantiate(_projectilePrefab);
-		//obj.transform.parent = transform;
-		obj.SetActive(false);
-		obj.gameObject.tag = _bulletOwner.ToString();
-		obj.GetComponent<Projectile>()._damage = _projectileDamage;
-		_projectilePool.Add(obj);
+		obj.SetActive(false); // Отключаем его чтобы он не отрисовывался и не обновлялся
+		obj.gameObject.tag = _bulletOwner.ToString(); // присваиваем тэг для этой пули
+		obj.GetComponent<Projectile>()._damage = _projectileDamage; // Устанавливаем урон наносимый этой пулей
+		_projectilePool.Add(obj); // Добавляем её в пул
 
-		return obj;
+		return obj; // и возвращаем её вызывающей функции
 	}
 
+	// Эта функция вызывается Unity когда объект уничтожен
 	void OnDestroy()
 	{
+		// Для каждой пули в списке
 		foreach(var obj in _projectilePool)
 		{
-			if(obj == null)
-				continue;
+			if(obj == null) // если она не существует
+				continue; // пропустить её
 			
 			var proj = obj.GetComponent<Projectile>();
 
