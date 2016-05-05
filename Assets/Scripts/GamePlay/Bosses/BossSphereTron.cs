2bcef4202ade86e6746ad0ef7f554ea71950e3f2
Piggos <randomdeiy@yandex.ru>
2016-05-05 12:48:10 +0300

    Добавил босса, но не доделал.
    Начал делать диалоговую систему, сделал создание файла с данными
    диалога.

---
+++ Assets/Scripts/GamePlay/Bosses/BossSphereTron.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossSphereTron : MonoBehaviour {
+
+	public Transform _spinningBody;
+	public float _spinSpeed;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (_spinningBody == null)
+		{
+			Debug.LogError ("Не найден корпус корабля босса");
+			enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_spinningBody.Rotate(new Vector3 (0f, 0f, _spinSpeed * Time.deltaTime));
+	}
+}
---
+++ Assets/Scripts/UI/Dialog.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public enum Character
+{
+	Tyan,
+	Kun,
+	Burrito
+}
+
+[System.Serializable]
+public struct Phrase
+{
+	public string _text;
+	public AudioClip _audio;
+}
+
+[System.Serializable]
+public struct Message
+{
+	public Character _speaker;
+	public List<Phrase> _phrases;
+	
+}
+
+[CreateAssetMenu()]
+public class Dialog : ScriptableObject
+{
+	public List<Message> _messages;
+}
