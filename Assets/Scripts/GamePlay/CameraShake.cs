be9b0438b9550a630e98e244b2960ea6dd675da2
Sergeeeek <Sergeeeekg@gmail.com>
2016-05-07 19:50:16 +0300

    Описал классы CameraShake и Health

--- Assets/Scripts/GamePlay/CameraShake.cs
+++ Assets/Scripts/GamePlay/CameraShake.cs
@@ -1,54 +1,91 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
+/// <summary>
+/// Данные использующиеся для тряски
+/// </summary>
 public struct ShakeData
 {
+    // Время начала тряски
     public float _shakeStart;
+    // Как долго длится тряска
     public float _shakeDuration;
+    // Сила тряски
     public float _shakeStrength;
+    // Случайный параметр для функции шума
     public float _noiseRandom;
 }
 
+/// <summary>
+/// Компонент отвечающий за тряску камеры при попаданиях, взрывах и т.д.
+/// </summary>
 public class CameraShake : MonoBehaviour
 {
+    [Tooltip("Кривая для смягчения начала и конца тряски.")]
     public AnimationCurve _startEndEasing;
 
+    // Список данных для тряски, чтобы можно было трясти камеру сразу несколькими разными способами
     public List<ShakeData> _shakeQueue;
 
     void Start()
     {
+        // Инициализация
         _shakeQueue = new List<ShakeData>();
     }
 
-    void Update()
-    {
-        for(int i = _shakeQueue.Count - 1; i >= 0; i--)
-        {
-            var shake = _shakeQueue[i];
-            if (Time.time - shake._shakeStart > shake._shakeDuration)
-            {
-                _shakeQueue.RemoveAt(i);
-                continue;
-            }
-
-            var easing = _startEndEasing.Evaluate((Time.time - shake._shakeStart) / shake._shakeDuration);
-            var noise = Mathf.PerlinNoise(Time.time - shake._shakeStart + shake._noiseRandom, 0f) * shake._shakeStrength * easing;
-            var rand = Random.insideUnitCircle * noise;
-            transform.position += new Vector3(rand.x, rand.y) * Time.deltaTime;
-            var tempz = transform.localPosition.z;
-            var backforce = transform.localPosition * easing;
-            backforce.z = tempz;
-            transform.localPosition = backforce;
-        }
-    }
-
+    /// <summary>
+    /// Публичная функция для запросов на тряску
+    /// </summary>
+    /// <param name="duration">Сколько тряска длится</param>
+    /// <param name="strength">Сила тряски</param>
     public void Shake(float duration, float strength)
     {
+        if (duration == 0f || strength == 0f)
+            return;
+
+        // Создаём и заполняем данные
         var data = new ShakeData();
         data._shakeStart = Time.time;
         data._shakeDuration = duration;
         data._shakeStrength = strength;
+        // Случайный параметр для функции шума, чтобы тряски отличались друг от друга
         data._noiseRandom = Random.Range(0f, 100000f);
         _shakeQueue.Add(data);
+    }
+
+    void Update()
+    {
+        // Итерация листа с конца к началу, т.к. в теле цикла может произойти удаление элемента листа
+        for(int i = _shakeQueue.Count - 1; i >= 0; i--)
+        {
+            var shake = _shakeQueue[i];
+            // Если время тряски закончилось
+            if (Time.time - shake._shakeStart > shake._shakeDuration)
+            {
+                // Удаляем её и продолжаем к следующему элементу цикла
+                _shakeQueue.RemoveAt(i);
+                continue;
+            }
+
+            // Получаем прогресс текущей тряски как значение от 0 до 1
+            var t = (Time.time - shake._shakeStart) / shake._shakeDuration;
+            // Подставляем прогресс в кривую из инспектора и получаем значение смягчения тряски
+            var easing = _startEndEasing.Evaluate(t);
+
+            // Используем функцию шума перлина с параметром: время с начала тряски + случайное число по x, умножаем на силу тряски и смягчение
+            var noise = Mathf.PerlinNoise(Time.time - shake._shakeStart + shake._noiseRandom, 0f) * shake._shakeStrength * easing;
+            // получаем случайный единичный вектор и умножаем его на шум
+            var rand = Random.insideUnitCircle * noise;
+            // Двигаем камеру в полученном направлении
+            transform.position += new Vector3(rand.x, rand.y) * Time.deltaTime;
+
+            // Запоминаем z позицию
+            var tempz = transform.localPosition.z;
+            // Умножаем позицию камеры на смягчение [0..1], для того чтобы камера плавно возвращалась в свое положение
+            var backforce = transform.localPosition * easing;
+            // Восстанавливаем z позицию
+            backforce.z = tempz;
+            transform.localPosition = backforce;
+        }
     }
 }
--- Assets/Scripts/GamePlay/Health.cs
+++ Assets/Scripts/GamePlay/Health.cs
@@ -2,17 +2,36 @@
 
 public class Health : MonoBehaviour
 {
+    [Tooltip("Макс. уровень здоровья")]
 	public float _maxHealth = 100f;
-	public float _health;
+    [Tooltip("Префаб который будет создаваться когда этот объект погибнет")]
 	public GameObject _deathEffect;
+    [Tooltip("Регенерировать здоровье")]
 	public bool _regenHealth;
+    [Tooltip("Сколько здоровья восстанавливать в секунду")]
 	public float _regenAmountPerSecond;
+    [Tooltip("Время после последнего нанесения урона до начала регенерации в сек.")]
 	public float _timeBeforeRegen;
 
+    [Tooltip("Множитель времени тряски камеры в секундах при нанесении урона.\nКол-во урона * множитель = время тряски.")]
+    public float _shakeDurationMultiplier;
+    [Tooltip("Множитель силы тряски камеры при нанесении урона.\nКол-во урона * множитель = сила тряски.")]
+    public float _shakeIntensityMultiplier;
+
+    // Время последнего нанесения урона
 	float _lastDamageTime;
+    // Текущий уровень здоровья
+    float _health;
+
+    // Указатель на компонент тряски камеры
+    CameraShake _shake;
 
     void Start()
     {
+        // Находим компонент тряски для будущего использования
+        _shake = FindObjectOfType<CameraShake>();
+
+        // В начале здоровье максимально
         _health = _maxHealth;
     }
 
@@ -20,27 +39,45 @@ public class Health : MonoBehaviour
 	{
 		if (_regenHealth)
 		{
+            // Если прошло _timeBeforeRegen сек. с предыдущего урона
 			if (Time.time - _lastDamageTime > _timeBeforeRegen)
 			{
+                // Здоровье = математический минимум между максимальным здоровьем и здоровьем после регенерации
 				_health = Mathf.Min(_health + _regenAmountPerSecond * Time.deltaTime, _maxHealth);
 			}
 		}
 	}
 
-	public void UpdateHealth(float health)
+    /// <summary>
+    /// Публичная функция для изменения здоровья из вне
+    /// </summary>
+    /// <param name="deltaHealth">сколько здоровья прибавить</param>
+	public void UpdateHealth(float deltaHealth)
 	{
-		_health += health;
+		_health += deltaHealth;
 
-		if (health < 0)
+        // Если нанесён урон
+		if (deltaHealth < 0)
 		{
+            // Трясём камеру
+            _shake.Shake(_shakeDurationMultiplier, _shakeIntensityMultiplier);
+            // Запоминаем время последнего нанесения урона
 			_lastDamageTime = Time.time;
 		}
 
 		if(_health <= 0f)
 		{
-			var obj = Instantiate(_deathEffect);
-			obj.transform.position = transform.position;
-			obj.transform.rotation = transform.rotation;
+            // Если префаб эффекта назначен
+            if(_deathEffect != null)
+            {
+                // Создаём его
+                var obj = Instantiate(_deathEffect);
+                // Ставим его в положение текущего объекта
+                obj.transform.position = transform.position;
+                obj.transform.rotation = transform.rotation;
+            }
+			
+            // Уничтожаем текущий объект
 			Destroy(gameObject);
 		}
 	}
