2f71c8d5368ed214b79235bd6c29f718b3416112
Sergeeeek <Sergeeeekg@gmail.com>
2016-05-11 15:57:19 +0300

    Куча изменений и баг фиксов

--- Assets/Scripts/GamePlay/DieOnParticleStop.cs
+++ Assets/Scripts/GamePlay/DieOnParticleStop.cs
@@ -5,6 +5,7 @@ using System.Linq;
 public class DieOnParticleStop : MonoBehaviour
 {
 	List<ParticleSystem> _particleSystems;
+    public bool _disable;
 
 	void Start()
 	{
@@ -14,6 +15,16 @@ public class DieOnParticleStop : MonoBehaviour
 	void Update()
 	{
 		if(_particleSystems.Count == 0 || _particleSystems.All(x => x.isStopped))
-			Destroy(gameObject);
+        {
+            if(_disable)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+			
 	}
 }
